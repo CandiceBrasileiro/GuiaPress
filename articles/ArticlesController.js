f7544671573457eabf875acea07822e6bcc33c37
CandiceBrasileiro <candicelopes88@gmail.com>
2023-01-14 10:45:00 -0300

    Parte de artigos e tinymce editável

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -51,4 +51,42 @@ router.post("/articles/delete", (req, res) => {
       res.redirect("/admin/articles");
   }
 });
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id;
+  Article.findByPk(id).then(article => {
+    if(article != undefined){
+      Category.findAll().then(categories => {
+        res.render("admin/articles/edit", {categories: categories, article: article})
+      })
+    }else{
+      res.redirect("/");
+    }
+  }).catch(erro => {
+    res.redirect("/");
+  })
+})
+
+router.post("/articles/update", (req, res) => { // UPDATE
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var categoryId = req.body.category;
+
+  Article.update(
+      {
+          title: title,
+          slug: slugify(title),
+          body: body,
+          categoryId: categoryId
+      }, {
+      where: {
+          id: id
+      }
+  }).then(() => {
+      res.redirect("/admin/articles");
+  });
+
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -45,6 +45,46 @@ app.get("/", (req, res) => {
 });
 })
 
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug:slug
+    }
+  }).then(article => {
+    if(article != undefined ){
+      Category.findAll().then(categories => {
+        res.render("article",  {article: article, categories: categories});
+    });
+    }else{
+      res.redirect("/");
+    }
+  }).catch( err => {
+    res.redirect("/");
+  });
+})
+
+app.get("/category/:slug", (req, res) =>{
+  var slug = req.params.slug;
+  Category.findOne({
+    where: {
+      slug: slug
+    },
+    include: [{model: Article}]
+  }).then(category => {
+    if(category != undefined){
+
+      Category.findAll().then(categories => {
+        res.render("index", {articles: category.articles, categories: categories})
+      });
+    }else{
+      res.redirect("/");
+    }
+  }).catch( err => {
+    res.redirect("/");
+  })
+})
+
 app.listen(8080, () => {
   console.log("O servidor está rodando!")
 })
